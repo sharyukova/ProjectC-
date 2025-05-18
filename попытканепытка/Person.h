d8ad3253dfa904af39e80309d315f4ed669c5f38
eelizavetochkaa <liza.bogdanova29@gmail.com>
2025-05-18 23:05:14 +0300

    еще больше нового в матрице судьбы

--- попытканепытка/Person.h
+++ попытканепытка/Person.h
@@ -39,6 +39,10 @@ namespace попытканепытка {
 	private: System::Windows::Forms::PictureBox^ matrixOfPerson;
 	private: System::Windows::Forms::Label^ A;
 	private: System::Windows::Forms::Label^ B;
+	private: System::Windows::Forms::Label^ C;
+	private: System::Windows::Forms::Label^ D;
+	private: System::Windows::Forms::Label^ E;
+
 	protected:
 
 	protected:
@@ -62,6 +66,9 @@ namespace попытканепытка {
 			this->matrixOfPerson = (gcnew System::Windows::Forms::PictureBox());
 			this->A = (gcnew System::Windows::Forms::Label());
 			this->B = (gcnew System::Windows::Forms::Label());
+			this->C = (gcnew System::Windows::Forms::Label());
+			this->D = (gcnew System::Windows::Forms::Label());
+			this->E = (gcnew System::Windows::Forms::Label());
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->matrixOfPerson))->BeginInit();
 			this->SuspendLayout();
 			// 
@@ -112,11 +119,12 @@ namespace попытканепытка {
 			this->A->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 21.75F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(204)));
 			this->A->ForeColor = System::Drawing::SystemColors::ControlText;
-			this->A->Location = System::Drawing::Point(166, 572);
+			this->A->Location = System::Drawing::Point(163, 572);
 			this->A->Name = L"A";
 			this->A->Size = System::Drawing::Size(31, 33);
 			this->A->TabIndex = 3;
 			this->A->Text = L"0";
+			this->A->Click += gcnew System::EventHandler(this, &Person::A_Click);
 			// 
 			// B
 			// 
@@ -125,11 +133,52 @@ namespace попытканепытка {
 				static_cast<System::Int32>(static_cast<System::Byte>(234)));
 			this->B->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 21.75F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(204)));
-			this->B->Location = System::Drawing::Point(388, 354);
+			this->B->Location = System::Drawing::Point(387, 354);
 			this->B->Name = L"B";
 			this->B->Size = System::Drawing::Size(31, 33);
 			this->B->TabIndex = 4;
 			this->B->Text = L"0";
+			// 
+			// C
+			// 
+			this->C->AutoSize = true;
+			this->C->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(250)), static_cast<System::Int32>(static_cast<System::Byte>(180)),
+				static_cast<System::Int32>(static_cast<System::Byte>(165)));
+			this->C->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 21.75F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->C->Location = System::Drawing::Point(601, 572);
+			this->C->Name = L"C";
+			this->C->Size = System::Drawing::Size(31, 33);
+			this->C->TabIndex = 5;
+			this->C->Text = L"0";
+			this->C->Click += gcnew System::EventHandler(this, &Person::C_Click);
+			// 
+			// D
+			// 
+			this->D->AutoSize = true;
+			this->D->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(250)), static_cast<System::Int32>(static_cast<System::Byte>(180)),
+				static_cast<System::Int32>(static_cast<System::Byte>(165)));
+			this->D->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 21.75F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->D->Location = System::Drawing::Point(382, 787);
+			this->D->Name = L"D";
+			this->D->Size = System::Drawing::Size(31, 33);
+			this->D->TabIndex = 6;
+			this->D->Text = L"0";
+			this->D->Click += gcnew System::EventHandler(this, &Person::D_Click);
+			// 
+			// E
+			// 
+			this->E->AutoSize = true;
+			this->E->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(205)), static_cast<System::Int32>(static_cast<System::Byte>(215)),
+				static_cast<System::Int32>(static_cast<System::Byte>(200)));
+			this->E->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 21.75F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->E->Location = System::Drawing::Point(382, 572);
+			this->E->Name = L"E";
+			this->E->Size = System::Drawing::Size(31, 33);
+			this->E->TabIndex = 7;
+			this->E->Text = L"0";
 			// 
 			// Person
 			// 
@@ -138,6 +187,9 @@ namespace попытканепытка {
 			this->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"$this.BackgroundImage")));
 			this->BackgroundImageLayout = System::Windows::Forms::ImageLayout::Stretch;
 			this->ClientSize = System::Drawing::Size(1440, 1100);
+			this->Controls->Add(this->E);
+			this->Controls->Add(this->D);
+			this->Controls->Add(this->C);
 			this->Controls->Add(this->B);
 			this->Controls->Add(this->A);
 			this->Controls->Add(this->button1);
@@ -160,6 +212,9 @@ namespace попытканепытка {
 		int sum = 0;
 		if (day == 11 || day == 22) {
 			sum = day;  
+		}
+		else if (day == 29) {
+			sum = 11;
 		}
 		else {
 			int tempDay = day;
@@ -200,8 +255,118 @@ namespace попытканепытка {
 		}
 		int year = selectedDate.Year;
 		int sum3 = 0;
+		int sum4 = 0;
+		int year1 = year / 100;
+		int year2 = year % 100;
+		if (year1 == 11) {
+			sum3 = year1;
+		}
+		else {
+			int tempYear1 = year1;
+			while (tempYear1 > 0) {
+				sum3 += tempYear1 % 10;
+				tempYear1 /= 10;
+			}
+			if (sum3 >= 10) {
+				int newSum3 = 0;
+				int tempSum3 = sum3;
+				while (tempSum3 > 0) {
+					newSum3 += tempSum3 % 10;
+					tempSum3 /= 10;
+				}
+				sum3 = newSum3;
+			}
+		}
+		if (year2 == 11) {
+			sum4 = year1;
+		}
+		else {
+			int tempYear2 = year2;
+			while (tempYear2 > 0) {
+				sum4 += tempYear2 % 10;
+				tempYear2 /= 10;
+			}
+			if (sum4 >= 10) {
+				int newSum4 = 0;
+				int tempSum4 = sum4;
+				while (tempSum4 > 0) {
+					newSum4 += tempSum4 % 10;
+					tempSum4 /= 10;
+				}
+				sum4 = newSum4;
+			}
+		}
+		int year3 = sum3+sum4;
+		int sum5 = 0;
+		if (year3 == 11) {
+			sum5 = year3;
+		}
+		else if (year3 == 29) {
+			sum5 = 11;
+		}
+		else {
+			int tempYear3 = year3;
+			while (tempYear3 > 0) {
+				sum5 += tempYear3 % 10;
+				tempYear3 /= 10;
+			}
+			if (sum5 >= 10) {
+				int newSum5 = 0;
+				int tempSum5 = sum5;
+				while (tempSum5 > 0) {
+					newSum5 += tempSum5 % 10;
+					tempSum5 /= 10;
+				}
+				sum5 = newSum5;
+			}
+		}
+		int sum6 = sum + sum2 + sum5;
+		int sum7 = 0;
+		if (sum6 == 11||sum6==22) {
+			sum7 = sum6;
+		}
+		else {
+			int temp = sum6;
+			while (temp> 0) {
+				sum7 += temp % 10;
+				temp /= 10;
+			}
+			if (sum7 >= 10) {
+				int newSum = 0;
+				int tempSum = sum7;
+				while (tempSum > 0) {
+					newSum += tempSum % 10;
+					tempSum /= 10;
+				}
+				sum7 = newSum;
+			}
+		}
+		int sumE = sum + sum2 + sum5 + sum7;
+		int result = 0;
+		if (sumE == 11 || sumE == 22) {
+			result = sumE;
+		}
+		else {
+			int temp = sumE;
+			while (temp > 0) {
+				result += temp % 10;
+				temp /= 10;
+			}
+			if (result >= 10) {
+				int newSum = 0;
+				int tempSum = result;
+				while (tempSum > 0) {
+					newSum += tempSum % 10;
+					tempSum /= 10;
+				}
+				result = newSum;
+			}
+		}
 		this->A->Text = sum.ToString();
 		this->B->Text = sum2.ToString();
+		this->C->Text = sum5.ToString();
+		this->D->Text = sum7.ToString();
+		this->E->Text = result.ToString();
 	}
 private: System::Void Person_Load(System::Object^ sender, System::EventArgs^ e) {
 	
@@ -210,6 +375,12 @@ private: System::Void Person_Load(System::Object^ sender, System::EventArgs^ e) 
 	matrixOfPerson->Region = gcnew System::Drawing::Region(path);
 }
 private: System::Void matrixOfPerson_Click(System::Object^ sender, System::EventArgs^ e) {
+}
+private: System::Void D_Click(System::Object^ sender, System::EventArgs^ e) {
+}
+private: System::Void A_Click(System::Object^ sender, System::EventArgs^ e) {
+}
+private: System::Void C_Click(System::Object^ sender, System::EventArgs^ e) {
 }
 };
 }
