3c28757af3c84096f3448bffc3ab765af9ffb12d
eelizavetochkaa <liza.bogdanova29@gmail.com>
2025-05-29 19:06:39 +0300

    ссылка выходит на другую инфу

---
+++ попытканепытка/LinkMessageBox.h
@@ -0,0 +1,79 @@
+#pragma once
+
+namespace попытканепытка {
+
+    using namespace System;
+    using namespace System::ComponentModel;
+    using namespace System::Collections;
+    using namespace System::Windows::Forms;
+    using namespace System::Data;
+    using namespace System::Drawing;
+
+    public ref class LinkMessageBox : public System::Windows::Forms::Form
+    {
+    public:
+        LinkMessageBox(void)
+        {
+            InitializeComponent();
+        }
+
+        void SetMessage(String^ text, String^ url) {
+            richTextBox1->Text = text + " " + url;
+
+            int linkStart = text->Length + 1;
+            richTextBox1->Select(linkStart, url->Length);
+            richTextBox1->SelectionColor = Color::Blue;
+            richTextBox1->SelectionFont = gcnew System::Drawing::Font(
+                richTextBox1->Font,
+                FontStyle::Underline
+            );
+            richTextBox1->Select(0, 0);
+        }
+
+    protected:
+        ~LinkMessageBox()
+        {
+            if (components)
+            {
+                delete components;
+            }
+        }
+
+    private:
+        System::Windows::Forms::RichTextBox^ richTextBox1;
+        System::ComponentModel::Container^ components;
+
+#pragma region Windows Form Designer generated code
+        void InitializeComponent(void)
+        {
+            this->richTextBox1 = (gcnew System::Windows::Forms::RichTextBox());
+            this->SuspendLayout();
+            this->richTextBox1->Dock = System::Windows::Forms::DockStyle::Fill;
+            this->richTextBox1->ReadOnly = true;
+            this->richTextBox1->Cursor = Cursors::Hand;
+            this->richTextBox1->Name = L"richTextBox1";
+            this->richTextBox1->LinkClicked += gcnew LinkClickedEventHandler(this, &LinkMessageBox::richTextBox1_LinkClicked);
+            // 
+            // LinkMessageBox
+            // 
+            this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+            this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+            this->ClientSize = System::Drawing::Size(300, 150);
+            this->Controls->Add(this->richTextBox1);
+            this->Name = L"LinkMessageBox";
+            this->Text = L"Информация";
+            this->ResumeLayout(false);
+        }
+#pragma endregion
+
+    private:
+        System::Void richTextBox1_LinkClicked(System::Object^ sender,
+            System::Windows::Forms::LinkClickedEventArgs^ e)
+        {
+            if (!String::IsNullOrEmpty(e->LinkText))
+            {
+                System::Diagnostics::Process::Start(e->LinkText);
+            }
+        }
+    };
+}
